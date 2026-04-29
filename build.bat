f19466b108a335ecb9f7127b6635b924da80dac1
Presley Poon <presleypoon37@gmail.com>
2026-04-29 21:57:54 +0100

    Simplify build.bat by removing echo statements
    Removed unnecessary echo statements and improved build script.

--- build.bat
+++ build.bat
@@ -1,12 +1,7 @@
-@echo off
-echo --- BATTENING DOWN THE HATCHES ---
 gcc Scavenger.c -o Scavenger.exe
-
 if %errorlevel% neq 0 (
     echo [!] Compilation failed. Take a screenshot and DM me!
     pause
     exit /b
 )
-
-echo [+] Build successful! Launching Scavenger...
 Scavenger.exe
